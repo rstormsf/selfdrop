0a821d2c4b174c620be9b8a02a5c69b5f4263885
Roman Storm <rstormsf@gmail.com>
2018-10-16 00:33:39 -0700

    init

--- contracts/SelfDrop.sol
+++ contracts/SelfDrop.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.4.25;
 
 interface IERC20 {
-  function balanceOf(address who) external view returns (uint256);
   function transfer(address to, uint256 value) external returns (bool);
 }
 
